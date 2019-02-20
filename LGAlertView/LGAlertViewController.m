b71b41d97c92e5bbe4b69a121a8f933c82f92628
Terence Lee <terencelee1234@gmail.com>
2019-02-20 11:23:39 +0800

    Update LGAlertViewController.m
    fix possible crash

--- LGAlertView/LGAlertViewController.m
+++ LGAlertView/LGAlertViewController.m
@@ -56,8 +56,10 @@
 
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [UIView animateWithDuration:coordinator.transitionDuration animations:^{
-            [self setNeedsStatusBarAppearanceUpdate];
-            [self.alertView layoutValidateWithSize:size];
+            if (self != nil && self.alertView != nil) {
+                [self setNeedsStatusBarAppearanceUpdate];
+                [self.alertView layoutValidateWithSize:size];
+            }
         }];
     });
 }
